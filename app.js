97e4cd2fc045bdca6900e116b1ee8f5df42ef2b5
immanuel <immanuelajimah@gmail.com>
2023-07-10 12:39:47 +0100

    Implemented google auth and protected route

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
 const morgan = require('morgan')
+const session = require('express-session')
 const { engine } = require('express-handlebars')
 const path = require('path')
+
+const passport = require('passport')
+
+//load config
 dotenv.config({ path: './config/config.env' })
+
+//passport config
+require('./config/passport')(passport)
 
 connectDB()
 const app = express()
@@ -19,14 +27,28 @@ app.engine('.hbs', engine({ extname: '.hbs' }))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 
-//static folder
+//session
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }),
+)
+
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+//port
+const PORT = process.env.PORT || 5000
 
 //Routes
 app.use('/', require('./routes/index'))
-const PORT = process.env.PORT || 5000
+app.use('/auth', require('./routes/auth'))
 
-//port
 app.listen(
   PORT,
   console.log(`server running in${process.env.NODE_ENV}mode on port ${PORT}`),
---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
+//login / auth with google
+
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
+
+/// google auth callback
+
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard')
+  },
+)
+
+//log out user
+router.get('/logout', (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/')
+  })
+})
+module.exports = router
